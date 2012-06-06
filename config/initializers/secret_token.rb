f41188886ed6345b00c13568e982af3d5684640d
James McKinney <james@slashpoundbang.com>
2012-06-06 17:27:11 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CitizenBudget::Application.config.secret_token = '39ab678a3f5fd14e3a4a5d25846f88a1259b876d858430613185d9d2811c5849c303b677732ee23900c5313b0e5f3afa0e61d250e5274ae37e4d3fdd980eee3d'
