66063531a41da0e5d4f32bc2c35b84065b782017
James McKinney <james@slashpoundbang.com>
2012-06-09 23:19:45 -0400

    fix automatic mapping of mongo

--- config/initializers/monkey_patches.rb
+++ config/initializers/monkey_patches.rb
@@ -1,3 +1,70 @@
+module Mongoid::Document
+  # Targets:
+  # :date (:date_select)
+  # :datetime (:datetime_select)
+  # :file
+  # :number
+  # :select
+  # :string
+  # :time (:time_select)
+
+  # Targets not used in +default_input_type+:
+  # :boolean
+  # :check_boxes
+  # :hidden
+  # :radio
+  # :range
+  # :text
+
+  # Targets based on attribute name:
+  # :country
+  # :email
+  # :password
+  # :phone
+  # :search
+  # :time_zone
+  # :url
+
+  COLUMN_TYPE_MAP = {
+    BigDecimal   => :number,
+    Float        => :number,
+    Integer      => :number,
+    Range        => :range,
+    TimeWithZone => :time,
+
+    # These don't map well (or even transform well):
+    # Array
+    # Hash
+
+    # These can be strings:
+    # BSON::ObjectId
+    # Regexp
+    # Symbol
+
+    # These transform to ActiveRecord types:
+    # Boolean
+    # Date
+    # DateTime
+    # String
+    # Time
+
+    # In Formtastic 2.2, uncomment:
+    # Date     => :date_select,
+    # DateTime => :datetime_select,
+    # Time     => :time_select,
+  }
+
+  Column = Struct.new :name, :type
+  def column_for_attribute(attribute)
+    name = attribute.to_s
+    type = self.class.fields[name].type
+    Column.new({
+      name: name,
+      type: COLUMN_TYPE_MAP[type] || type.to_s.downcase.to_sym,
+    })
+  end
+end
+
 module ActiveAdmin
   module Views
     module Pages
