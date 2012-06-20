be969a88d06325dc66cc05fe1122f0a8b06141fa
James McKinney <james@slashpoundbang.com>
2012-06-20 13:18:17 -0400

    add bitly api model

---
+++ app/models/bitly.rb
@@ -0,0 +1,18 @@
+class Bitly
+  class << self
+    # @return [Faraday::Connection] an HTTP client
+    def client
+      Faraday.new 'https://api-ssl.bitly.com' do |builder|
+        builder.request :url_encoded
+        builder.response :json
+        builder.adapter :net_http
+      end
+    end
+
+    # Shortens a URL.
+    # @see http://dev.bitly.com/links.html#v3_shorten
+    def shorten(url)
+      client.get('/v3/shorten', longUrl: url, login: ENV['BITLY_LOGIN'], apiKey: ENV['BITLY_API_KEY']).body['data']['url']
+    end
+  end
+end
