d6c8269bcae55111e0c54586acb6081bb8186f12
James McKinney <james@slashpoundbang.com>
2012-06-19 20:31:27 -0400

    update assets

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,6 @@ module CitizenBudget
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.5'
+    config.assets.version = '1.6'
   end
 end
