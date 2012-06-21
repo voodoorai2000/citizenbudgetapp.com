aec9ce469474fd9299dd7e95d663e4c499cf2bae
James McKinney <james@slashpoundbang.com>
2012-06-21 12:23:29 -0400

    fix error in display_name

--- app/models/response.rb
+++ app/models/response.rb
@@ -46,7 +46,7 @@ class Response
   # @returns [String] the first name and last name initial
   def display_name
     if name?
-      parts = name.split(' ', 2)
+      parts = name.strip.split(' ', 2)
       parts[0] = UnicodeUtils.titlecase(parts[0]) if parts[0][/\A\p{Ll}/]
       parts[1] = "#{UnicodeUtils.upcase(parts[1][0])}." if parts[1]
       parts.join ' '
