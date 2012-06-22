f8b2c4d3e477c410f65fc033eab0da518e9f8a88
James McKinney <james@slashpoundbang.com>
2012-06-22 15:39:42 -0400

    update active admin, use page not dashboard

---
+++ app/admin/dashboard.rb
@@ -0,0 +1,45 @@
+ActiveAdmin.register_page "Dashboard" do
+  menu :priority => 1
+
+  content do
+    if Questionnaire.active.count.nonzero?
+      column t(:active_consultations) do
+        ul do
+          Questionnaire.includes(:organization).active.each do |q|
+            li auto_link(q.organization) + ': ' + auto_link(q)
+          end
+        end
+      end
+    end
+
+    if Questionnaire.future.count.nonzero?
+      column t(:future_consultations) do
+        ul do
+          Questionnaire.includes(:organization).future.each do |q|
+            li auto_link(q.organization) + ': ' + auto_link(q)
+          end
+        end
+      end
+    end
+
+    # Here is an example of a simple dashboard with columns and panels.
+    #
+    # columns do
+    #   column do
+    #     panel "Recent Posts" do
+    #       ul do
+    #         Post.recent(5).map do |post|
+    #           li link_to(post.title, admin_post_path(post))
+    #         end
+    #       end
+    #     end
+    #   end
+
+    #   column do
+    #     panel "Info" do
+    #       para "Welcome to ActiveAdmin."
+    #     end
+    #   end
+    # end
+  end # content
+end
