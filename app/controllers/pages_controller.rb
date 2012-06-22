0dcd2f9225bc47b98ebc3389ab3090cd17406205
James McKinney <james@slashpoundbang.com>
2012-06-22 11:39:16 -0400

    render 404

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def channel
     render layout: false
   end
+
+  def not_found
+    render file: "#{Rails.root}/public/404.html", status: 404, layout: false
+  end
 end
