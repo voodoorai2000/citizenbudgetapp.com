6c7c793b28aa6d359c3406381a534fedf4ac494a
James McKinney <james@slashpoundbang.com>
2012-06-29 01:30:01 -0400

    update secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CitizenBudget::Application.config.secret_token = '39ab678a3f5fd14e3a4a5d25846f88a1259b876d858430613185d9d2811c5849c303b677732ee23900c5313b0e5f3afa0e61d250e5274ae37e4d3fdd980eee3d'
+CitizenBudget::Application.config.secret_token = ENV['SECRET_TOKEN']
