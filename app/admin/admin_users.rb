784ac7312a7fd63cb7469db50342243b98c7c03b
James McKinney <james@slashpoundbang.com>
2012-07-01 16:49:13 -0400

    expose organization and role in adminuser

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -5,6 +5,10 @@ ActiveAdmin.register AdminUser do
 
   index do
     column :email
+    column :organization
+    column :role do |a|
+      t(a.role, scope: :role) if a.role?
+    end
     column :locale do |a|
       Locale.locale_name(a.locale) if a.locale?
     end
@@ -14,6 +18,8 @@ ActiveAdmin.register AdminUser do
   form do |f|
     f.inputs do
       f.input :email
+      f.input :organization
+      f.input :role, as: :radio, collection: AdminUser::ROLES.map{|r| [t(r, scope: :role), r]}
       f.input :locale, as: :radio, collection: Locale::LOCALES.map{|k,v| [v, k]}
     end
     f.actions
@@ -22,6 +28,10 @@ ActiveAdmin.register AdminUser do
   show do
     attributes_table do
       row :email
+      row :organization
+      row :role do |a|
+        t(a.role, scope: :role) if a.role?
+      end
       row :locale do |a|
         Locale.locale_name(a.locale) if a.locale?
       end
