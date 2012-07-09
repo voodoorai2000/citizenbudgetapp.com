867fb8a860ad83ed3e97e3c14c53c91c0877ad6b
James McKinney <james@slashpoundbang.com>
2012-07-09 11:07:38 -0400

    make google client more robust

--- app/models/google_client.rb
+++ app/models/google_client.rb
@@ -1,8 +1,11 @@
 # http://code.google.com/p/google-api-ruby-client/wiki/OAuth2
+# @note Using a JavaScript client, it's impossible for users belonging to the
+# same organization to share access to Google Analytics data.
 class GoogleClient
   class ConfigurationError < StandardError; end
   class AccessRevokedError < StandardError; end
   class MissingRefreshToken < StandardError; end
+  class APIError < StandardError; end
 
   attr_reader :client
 
@@ -44,6 +47,7 @@ class GoogleClient
   # @return [Boolean] if the revocation was successful
   # @raises [MissingRefreshToken] if no refresh token
   # @see https://developers.google.com/accounts/docs/OAuth2WebServer#tokenrevoke
+  # @note Can revoke access at https://accounts.google.com/b/0/IssuedAuthSubTokens
   def revoke!
     if client.authorization.refresh_token
       Faraday.get('https://accounts.google.com/o/oauth2/revoke', token: client.authorization.refresh_token).status == 200
@@ -58,11 +62,10 @@ class GoogleClient
   end
 
   # Ensures the client has a fresh access token.
-  # @raises [Signet::AuthorizationError] if access was revoked
+  # @raises [AccessRevokedError] if access was revoked
   # @see https://github.com/sporkmonger/signet/blob/master/lib/signet/oauth_2/client.rb#L157
   # @see https://github.com/sporkmonger/signet/blob/master/lib/signet/oauth_2/client.rb#L581
   # @note If access is revoked while the token is fresh, no exception is raised.
-  # @note Can revoke access at https://accounts.google.com/b/0/IssuedAuthSubTokens
   def refresh!(options = {})
     client.authorization.update_token! options
     if client.authorization.refresh_token && client.authorization.expired?
@@ -77,15 +80,16 @@ class GoogleClient
   # @return a data object of the authenticated user's Google Analytics profiles
   # @see https://developers.google.com/apis-explorer/#p/analytics/v3/
   # @note Default max-results is 1000, which we are unlikely to exceed.
+  # @raises [AccessRevokedError] if access was revoked
+  # @raises [APIError] if a server or transmission error occurred
   def profiles
-    result = client.execute(client.discovered_api('analytics', 'v3').management.profiles.list, accountId: '~all', webPropertyId: '~all')
-    if result.status == 200
-      result.data
-    elsif [400, 401, 403].include? result.status
-      unauthorize!
-      # Raise an error so that the application knows to expire tokens.
-      raise AccessRevokedError
-    end
+    client.execute!(api_method: client.discovered_api('analytics', 'v3').management.profiles.list, parameters: { accountId: '~all', webPropertyId: '~all'}).data
+  rescue Google::APIClient::ClientError
+    unauthorize!
+    # Raise an error so that the application knows to expire tokens.
+    raise AccessRevokedError
+  rescue Google::APIClient::ServerError, Google::APIClient::TransmissionError
+    raise APIError
   end
 
   # @return [Signet::OAuth2::Client]
