219244734e7a636822df4b21319eaecb753fa8ae
James McKinney <james@slashpoundbang.com>
2012-07-20 18:42:45 -0400

    use appropriate locale in channelUrl

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ CitizenBudget::Application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   resources :responses, only: [:new, :create, :show]
-  match 'channel' => 'pages#channel', as: :channel, via: :get
+  match ':locale/channel' => 'pages#channel', as: :channel, via: :get
   match 'oauth2callback' => 'pages#oauth2callback', as: :oauth2callback
   root to: 'responses#new'
 
