1c609e11320e274c30279ff3ed7544772ba5c1ba
James McKinney <james@slashpoundbang.com>
2012-09-05 19:21:31 -0400

    precompile assets

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module CitizenBudget
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.30'
+    config.assets.version = '1.31'
 
     # https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar
     config.assets.initialize_on_precompile = false
