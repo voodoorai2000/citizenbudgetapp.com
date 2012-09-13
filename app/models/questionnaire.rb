cdbfb0f1298492f7fa9e2ffcc34b7f18a6c878d0
James McKinney <james@slashpoundbang.com>
2012-09-13 09:50:50 -0400

    relax length limit on instructions

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -67,7 +67,6 @@ class Questionnaire
   validates_inclusion_of :mode, in: MODES, allow_blank: true
   validates_inclusion_of :locale, in: Locale.available_locales, allow_blank: true
   validates_inclusion_of :time_zone, in: ActiveSupport::TimeZone.all.map(&:name), allow_blank: true
-  validates_length_of :instructions, maximum: 150, allow_blank: true
   validates_numericality_of :default_assessment, greater_than: 0, only_integer: true, allow_blank: true
   validates_numericality_of :tax_rate, greater_than: 0, less_than: 1, allow_blank: true
   validates_length_of :twitter_text, maximum: 140, allow_blank: true
