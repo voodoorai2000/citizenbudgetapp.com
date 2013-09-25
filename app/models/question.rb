315aef6f972458908c998fe8ba5721f4c642555e
James McKinney <james@slashpoundbang.com>
2013-09-25 14:52:11 -0400

    validate default value on sliders and scalers

--- app/models/question.rb
+++ app/models/question.rb
@@ -70,7 +70,7 @@ class Question
   validates_numericality_of :step, greater_than: 0, allow_blank: true, if: ->(q){%w(scaler slider).include?(q.widget)}
   validate :maximum_units_must_be_greater_than_minimum_units, if: ->(q){%w(scaler slider).include?(q.widget)}
   validate :default_value_must_be_between_minimum_and_maximum, if: ->(q){%w(scaler slider).include?(q.widget)}
-  validate :default_value_must_be_an_option, if: ->(q){q.widget == 'option'}
+  validate :default_value_must_be_an_option, if: ->(q){%w(scaler slider option).include?(q.widget)}
   validate :options_and_labels_must_agree, if: ->(q){q.widget == 'option'}
 
   after_initialize :get_options, :get_labels
@@ -257,7 +257,7 @@ private
   end
 
   def default_value_must_be_an_option
-    if widget == 'option' && options.present? && default_value.present?
+    if %w(scaler slider option).include?(widget) && options.present? && default_value.present?
       unless options.include?(default_value)
         errors.add :default_value, I18n.t('errors.messages.default_value_must_be_an_option')
       end
